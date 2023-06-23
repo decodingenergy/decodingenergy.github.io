be28307d26523a8c65a5a56bdc16cae291da389d
decodingenergy <125838009+decodingenergy@users.noreply.github.com>
2023-06-23 12:32:52 +0100

    Add files via upload

--- js/df1.js
+++ js/df1.js
@@ -10,7 +10,6 @@ $(document).ready(function(){
 
 
 
-
 filterSelection("allc")
 function filterSelection(c) {
   var x, i;
@@ -20,19 +19,6 @@ function filterSelection(c) {
   for (i = 0; i < x.length; i++) {
     w3RemoveClass(x[i], "show");
     if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
-  }
-}
-
-
-filterSelection2("allc")
-function filterSelection2(c) {
-  var x, i;
-  x = document.getElementsByClassName("filterDiv");
-  if (c == "all") c = "";
-  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
-  for (i = 0; i < x.length; i++) {
-    w3RemoveClass(x[i], "show2");
-    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show2");
   }
 }
 
@@ -72,45 +58,8 @@ for (var i = 0; i < btns.length; i++) {
     current[0].className = current[0].className.replace(" active2", "");
     this.className += " active2";
     $("img").hide();
-	$("." + $(this).val()).show();
-  });
-}
-
-});
-
-
-
-$(document).ready(function(){
-// Add active class to the current control button (highlight it)
-var btnContainer = document.getElementById("myBtnContainer2");
-var btns = btnContainer.getElementsByClassName("btn2");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active3");
-    current[0].className = current[0].className.replace(" active3", "");
-    this.className += " active3";
-    $("img").hide();
     $("." + $(this).val()).show();
   });
 }
 
 });
-
-$(document).ready(function(){
-// Add active class to the current control button (highlight it)
-var btnContainer = document.getElementById("myBtnContainer3");
-var btns = btnContainer.getElementsByClassName("btn3");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active4");
-    current[0].className = current[0].className.replace(" active4", "");
-    this.className += " active4";
-    $("img").hide();
-    $("." + $(this).val()).show();
-  });
-}
-
-});
-
-
-
